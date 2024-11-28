2fc8ae6f4abf6322a0a51a09dda74e17632ce760
Zlow-k <yj.kasai@gmail.com>
2024-11-28 23:13:01 +0900

    テンプレートを作成

--- apps/app.py
+++ apps/app.py
@@ -3,8 +3,13 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
+
+@app.route('/hello')
+def hello():
+    return render_template('hello.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
