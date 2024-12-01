59d2938a5fb1d40eda78d7e70c1ec49dbe2c4899
Zlow-k <yj.kasai@gmail.com>
2024-12-02 08:41:36 +0900

    CSS編集/aboutページ作成

--- apps/app.py
+++ apps/app.py
@@ -10,6 +10,9 @@ def index():
 def hello():
     return render_template('hello.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
